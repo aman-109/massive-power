d38c0e2715d7749426100fd0a5aeb9a15cccbf94
SaurabhKumar <srbhkumar624@gmail.com>
2022-12-22 15:21:24 +0530

    completed home page

--- stockby/pages/index.js
+++ stockby/pages/index.js
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import io from 'Socket.IO-client'
 
 export default function Home() {
-  useEffect(() => {socketInitializer()}, [])
+  // useEffect(() => {socketInitializer()}, [])
 
   // const socketInitializer = async () => {
   //   await fetch('/api/socket')
